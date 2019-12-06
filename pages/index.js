3c4377db1f0b186123a9d68f079d697d1f53575c
_andy_mz <asmz5488@gmail.com>
2019-12-06 11:32:04 -0500

    built House component

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,15 @@
+import houses from './houses.json'
+import House from '../components/House'
+
 const Index = () => (
   <div>
-    <h1>Nextbnb</h1>
+    <h2>Places to stay</h2>
+
+    <div className='houses'>
+      {houses.map((house, index) => {
+        return <House {...house} key={index} />
+      })}
+    </div>
   </div>
 )
 
