aa4c5d5b00bfba1bfc24ce3c8e1a6fb3f460c111
_andy_mz <asmz5488@gmail.com>
2019-12-07 10:42:10 -0500

    layout the whole project

--- pages/houses/[id].js
+++ pages/houses/[id].js
@@ -1,20 +1,19 @@
 import houses from '../houses.json'
 import Head from 'next/head'
+import Layout from '../../components/Layout'
 
 const House = props => (
-  <div>
-    <Head>
-      <title>{props.house.title}</title>
-    </Head>
-    <img src={props.house.picture} width='50%' alt='House picture' />
-    <p>
-      {props.house.type} - {props.house.town}
-    </p>
-    <p>{props.house.title}</p>
-    <p>
-      {props.house.rating} ({props.house.reviewsCount})
-    </p>
-  </div>
+  <Layout>
+    <div>
+      <Head>
+        <title>{props.house.title}</title>
+      </Head>
+      <img src={props.house.picture} width='100%' alt='House picture' />
+      <p>{props.house.type} - {props.house.town}</p>
+      <p>{props.house.title}</p>
+      <p>{props.house.rating} ({props.house.reviewsCount})</p>
+    </div>
+  </Layout>
 )
 
 House.getInitialProps = ({query}) => {
--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,28 @@
 import houses from './houses.json'
 import House from '../components/House'
+import Layout from '../components/Layout'
 
 const Index = () => (
-  <div>
-    <h2>Places to stay</h2>
+  <Layout>
+    <div>
+      <h2>Places to stay</h2>
 
-    <div className='houses'>
-      {houses.map((house, index) => {
-        return <House {...house} key={index} />
-      })}
+      <div className='houses'>
+        {houses.map((house, index) => {
+          return <House {...house} key={index} />
+        })}
+      </div>
+
+      <style jsx>{`
+        .houses {
+          display: grid;
+          grid-template-columns: 50% 50%;
+          grid-template-rows: 300px 300px;
+          grid-gap: 40px;
+        }
+      `}</style>
     </div>
-
-    <style jsx>{`
-      .houses {
-        display: grid;
-        grid-template-columns: 50% 50%;
-        grid-template-rows: 300px 300px;
-        grid-gap: 40px;
-      }
-    `}</style>
-  </div>
+  </Layout>
 )
 
 export default Index
